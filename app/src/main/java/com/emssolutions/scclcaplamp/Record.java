fd98737047716266dca147b96f90ca8c12cba8f6
maheshveldandi-rgb <maheshveldandi@gmail.com>
2026-08-16 07:33:34 +0530

    Add attendance record model

---
+++ app/src/main/java/com/emssolutions/scclcaplamp/Record.java
@@ -0,0 +1,16 @@
+package com.emssolutions.scclcaplamp;
+
+public class Record {
+    public long id;
+    public String date = "";
+    public String time = "";
+    public String action = "";
+    public String msn = "";
+    public String empId = "";
+    public String lampNo = "";
+    public String name = "";
+    public String designation = "";
+    public String mobile = "";
+    public String qrData = "";
+    public String remarks = "";
+}
